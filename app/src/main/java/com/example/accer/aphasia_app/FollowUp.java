74c040cf57d790d863c6d23361eb3ee4d91b3647
SudhindraNayak <mahi1995>
2017-06-21 15:54:21 +0530

    V2.10

---
+++ app/src/main/java/com/example/accer/aphasia_app/FollowUp.java
@@ -0,0 +1,127 @@
+package com.example.accer.aphasia_app;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
+
+import java.util.ArrayList;
+
+public class FollowUp extends AppCompatActivity {
+
+    private PieChart mChart1;
+    private SeekBar mSeekBarX, mSeekBarY;
+    private TextView tvX, tvY;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_follow_up);
+
+        mChart1 =(PieChart)findViewById(R.id.pieChart);
+        mChart1.setUsePercentValues(false);
+        mChart1.getDescription().setEnabled(false);
+        mChart1.setExtraOffsets(5, 10, 5, 5);
+
+        mChart1.setDragDecelerationFrictionCoef(0.95f);
+
+        mChart1.setCenterText(generateCenterSpannableText());
+
+        mChart1.setDrawHoleEnabled(true);
+        mChart1.setHoleColor(Color.WHITE);
+
+        mChart1.setTransparentCircleColor(Color.WHITE);
+        mChart1.setTransparentCircleAlpha(110);
+
+        mChart1.setHoleRadius(58f);
+        mChart1.setTransparentCircleRadius(61f);
+
+        mChart1.setDrawCenterText(true);
+
+        mChart1.setRotationAngle(0);
+        // enable rotation of the chart by touch
+        mChart1.setRotationEnabled(true);
+        mChart1.setHighlightPerTapEnabled(true);
+
+        setData(2);
+
+        mChart1.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+
+        Legend l = mChart1.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(0f);
+        l.setYOffset(0f);
+
+        // entry label styling
+        mChart1.setEntryLabelColor(Color.WHITE);
+        mChart1.setEntryLabelTextSize(12f);
+    }
+    private void setData(int count) {
+
+        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+
+            entries.add(new PieEntry(10,"ವಿಫಲವಾಗಿದೆ"));
+            entries.add(new PieEntry(20,"ಯಶಸ್ವಿ"));
+
+        PieDataSet dataSet = new PieDataSet(entries,"ಒಟ್ಟಾರೆ ಪ್ರಗತಿ");
+
+        dataSet.setDrawIcons(false);
+
+        dataSet.setSliceSpace(3f);
+        dataSet.setIconsOffset(new MPPointF(0, 40));
+        dataSet.setSelectionShift(5f);
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(15f);
+        data.setValueTextColor(Color.WHITE);
+        mChart1.setData(data);
+
+        // undo all highlights
+        mChart1.highlightValues(null);
+
+        mChart1.invalidate();
+    }
+    private SpannableString generateCenterSpannableText() {
+
+        SpannableString s = new SpannableString("MPAndroidChart\ndeveloped by Philipp Jahoda");
+        s.setSpan(new RelativeSizeSpan(1.7f), 0, 14, 0);
+        s.setSpan(new StyleSpan(Typeface.NORMAL), 14, s.length() - 15, 0);
+        s.setSpan(new ForegroundColorSpan(Color.GRAY), 14, s.length() - 15, 0);
+        s.setSpan(new RelativeSizeSpan(.8f), 14, s.length() - 15, 0);
+        s.setSpan(new StyleSpan(Typeface.ITALIC), s.length() - 14, s.length(), 0);
+        s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), s.length() - 14, s.length(), 0);
+        return s;
+    }
+}
